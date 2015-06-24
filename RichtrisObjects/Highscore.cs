8d6456c16ef4da9e308576a7d691e3c926bc728d
jodeka <joq@quast.oi>
2015-06-24 20:57:05 +0200

    Highscore-Layout gepimpt

--- RichtrisObjects/Highscore.cs
+++ RichtrisObjects/Highscore.cs
@@ -10,9 +10,9 @@ namespace RichtrisObjects
     public sealed class Highscore
     {
         private static readonly Highscore instanceBiatch = new Highscore();
+
         private Highscore() {
             this.Liste = new Dictionary<String, int>();
-        
         }
 
         public static Highscore Instance
@@ -42,14 +42,14 @@ namespace RichtrisObjects
                         select pair;
 
             int i = 0;
-            Console.WriteLine("Highscore\n___________");
+            Console.WriteLine("\n=============================- Highscore -=====================================\n");
             foreach (KeyValuePair<string, int> pair in items)
             {
                 i++;
 
-                Console.WriteLine(String.Format("--- {0} --- {1} --- {2} ", i, pair.Key, pair.Value));
+                Console.WriteLine(String.Format("--- {0,-5} --- {1,-15} --- {2,15} ", i, pair.Key, pair.Value));
             }
-            Console.WriteLine("_____________________\n");
+            Console.WriteLine("\n================================================================================ ");
 
         }
 
