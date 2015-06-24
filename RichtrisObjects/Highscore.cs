0dd7b356896c79683674999befbe3893ab92b738
DarkDragon <abc@gmx.de>
2015-06-24 19:56:03 +0200

    Highscoreliste

---
+++ RichtrisObjects/Highscore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RichtrisObjects
+{
+    public sealed class Highscore
+    {
+        private static readonly Highscore instanceBiatch = new Highscore();
+        private Highscore() { }
+
+        public static Highscore Instance
+        {
+            get
+            { return instanceBiatch; }
+
+
+        }
+
+        public Dictionary<String, int> Liste
+        {
+            get;
+
+        }
+    }
+}
+
